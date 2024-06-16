569b1524446f241c14316a86bf71b7ff88da25d9
bhoirprathmesh2004@gmail.com <bhoirprathmesh2004@gmail.com>
2024-06-16 15:14:52 +0530

    js

---
+++ JS2/01_basics/01_variables.js
@@ -0,0 +1,21 @@
+const accountId = 144553 
+let accountEmail = "Prathmesh@gmail.com"
+var accountPassword = "12345" // (now we are not using var)
+accountCity = "Jaipur" // (we can use this i.e without variable but not recommended to use)
+let accountState;
+
+// accountId = 2 (not allowed) --> if we use const keyword we cannot change the the value
+
+// accountEmail = "Nikita@gmail.com"
+
+// accountPassword = "67890"
+
+// accountCity = "Mumbai"
+
+// console.log(accountCity);
+
+/*
+NOTE : Prefer not to use var - because of issue in block scope and function scope.
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
