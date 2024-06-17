ca28bd3d868b014c692332e5e9cc3a8d9fa33b1a
bhoirprathmesh2004@gmail.com <bhoirprathmesh2004@gmail.com>
2024-06-17 11:15:51 +0530

    js

--- JS2/01_basics/01_variables.js
+++ JS2/01_basics/01_variables.js
@@ -18,4 +18,5 @@ let accountState;
 NOTE : Prefer not to use var - because of issue in block scope and function scope.
 */
 
-console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
+console.log(accountId, accountEmail, accountPassword, accountCity, accountState) //print in the normal format
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]); //print in the tabular format
---
+++ JS2/01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; 
+/* treat all js code as newer version (code will use strict mode) as now we not write this line as new version is already work on strict mode.
+*/
+
+//alert(3+3) -->we are using nodejs, not browser
+
+let name = "Pratham"
+let age = 18
+let isLoggedIn = false
+let state; 
+let city = null
+
+// primitive datatype :
+/*
+number => range - 2 to power 53
+bigint => when number is greater 
+string => ""
+boolean => true/false
+null => standalone value
+undefined =>
+symbol => unique
+*/
+
+//object
+console.log(typeof "pratham")
+console.log(typeof undefined)
+console.log(typeof null) //return object
