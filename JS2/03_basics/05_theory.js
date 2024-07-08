09cb9016da558e7fe312933def57c4ab73eda436
bhoirprathmesh2004@gmail.com <bhoirprathmesh2004@gmail.com>
2024-07-08 19:54:05 +0530

    js

--- JS2/03_basics/05_theory.js
+++ JS2/03_basics/05_theory.js
@@ -1,6 +1,13 @@
 /* 
 1. Execution Context
 
+JS file -->(it create) Global EC -->(refer to) this (value - window object)
+-->(also create) Function EC
+-->(also create) Eval Ec
+
+JS file code -->(runs in two phases)
+1. Memeory Creation Phase --> it create the memory for the variable which was declare in the js file
+2. Execution Phase
 
 2. Call Stack
 
