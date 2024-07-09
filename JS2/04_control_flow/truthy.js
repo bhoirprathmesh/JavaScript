9b476ba679a30e24e8170798788c26a17b121de2
bhoirprathmesh2004@gmail.com <bhoirprathmesh2004@gmail.com>
2024-07-09 12:12:01 +0530

    js

--- JS2/04_control_flow/truthy.js
+++ JS2/04_control_flow/truthy.js
@@ -7,15 +7,16 @@ if (userEmail) {
 }
 
 // falsy values
-
 // false, 0, -0, BigInt 0n, "", null, undefined, NaN
 
-//truthy values
+// truthy values
 // "0", 'false', " ", [], {}, function(){}
 
-// if (userEmail.length === 0) {
-//     console.log("Array is empty");
-// }
+if (userEmail.length === 0) {
+    console.log("Array is empty");
+}
+
+// above not understand
 
 const emptyObj = {}
 
@@ -23,7 +24,7 @@ if (Object.keys(emptyObj).length === 0) {
     console.log("Object is empty");
 }
 
-// Nullish Coalescing Operator (??): null undefined
+// Nullish Coalescing Operator (??): (only for) null and undefined
 
 let val1;
 // val1 = 5 ?? 10
